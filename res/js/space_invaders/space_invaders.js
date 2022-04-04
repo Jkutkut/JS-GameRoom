b6e008d764a4825e8694793160bd594f7fbc9f62
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-04 21:13:44 +0200

    sprites added for space invaders

--- res/js/space_invaders/space_invaders.js
+++ res/js/space_invaders/space_invaders.js
@@ -1,8 +1,16 @@
 function setup() {
 	createCanvas(600, 600);
 	background(0);
+
+	let size = 80;
+
+	let offsetH = size / 20, offsetV = size * 0.6;
+	for (let i = 0; i < 600 + size; i+=offsetH) {
+		for (let j = 0; j < 600; j+=offsetV) {
+			ellipse(i, j, size, size);
+		}
+	}
 }
 
 function draw() {
-	
 }
