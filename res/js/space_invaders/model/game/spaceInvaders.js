2acd9772505896f2550bb85165b6171c31f14070
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-18 11:15:55 +0200

    ship now on the center

--- res/js/space_invaders/model/game/spaceInvaders.js
+++ res/js/space_invaders/model/game/spaceInvaders.js
@@ -2,11 +2,18 @@ class SpaceInvaders extends Game {
 	constructor(size) {
 		super(size);
 
-		this.ship = new Ship(new p5.Vector(100, 100), new p5.Vector(50, 50), 0);
+		this.initShip();
 	}
 
 	show() {
 		super.show();
 		this.ship.show();
+	}
+
+	initShip() {
+		let offsetV = this.size.y / 50;
+		let shipSize = new p5.Vector(50, 50);
+		let shipPos = new p5.Vector((this.size.x - shipSize.x) / 2, this.size.y - offsetV - shipSize.y);
+		this.ship = new Ship(shipPos, shipSize, 0);
 	}
 }
