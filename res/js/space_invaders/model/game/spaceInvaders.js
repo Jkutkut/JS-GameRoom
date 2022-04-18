f423ee4ca4cff1b2d22f0207f2735057db887765
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-18 11:57:34 +0200

    enemies init method created

--- res/js/space_invaders/model/game/spaceInvaders.js
+++ res/js/space_invaders/model/game/spaceInvaders.js
@@ -3,11 +3,15 @@ class SpaceInvaders extends Game {
 		super(size);
 
 		this.initShip();
+		this.initEnemies();
 	}
 
 	show() {
 		super.show();
 		this.ship.show();
+		for (let enemy of this.enemies) {
+			enemy.show();
+		}
 	}
 
 	initShip() {
@@ -15,5 +19,15 @@ class SpaceInvaders extends Game {
 		let shipSize = new p5.Vector(50, 50);
 		let shipPos = new p5.Vector((this.size.x - shipSize.x) / 2, this.size.y - offsetV - shipSize.y);
 		this.ship = new Ship(shipPos, shipSize, 0);
+	}
+
+	initEnemies() {
+		this.enemies = [];
+		let basicSize = new p5.Vector(50, 50);
+
+		this.enemies.push(new BasicEnemy(
+			new p5.Vector(this.size.x / 4, this.size.y / 4),
+			basicSize.copy()
+		));
 	}
 }
