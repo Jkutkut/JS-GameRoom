e8d97076b1ebbdc4651fbde07bc8712ccc958064
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-18 11:16:27 +0200

    ready to add enemies

--- res/js/space_invaders/space_invaders.js
+++ res/js/space_invaders/space_invaders.js
@@ -1,4 +1,4 @@
-var ship;
+var game;
 
 function preload() {
 	Ship.SRC = loadImage("../res/img/space_invaders/ships/ship_1.png");
@@ -6,26 +6,13 @@ function preload() {
 
 function setup() {
 	createCanvas(600, 600);
-	ship = new Ship(new p5.Vector(100, 100), new p5.Vector(80, 80), 0);
-	ship.show();
-
+	game = new SpaceInvaders(new p5.Vector(width, height));
+	background(30);
 	// frameRate(60);
+
 }
 
 function draw() {
-	background(0);
-
-	let size = 80;
-
-	let offsetH = size / 20, offsetV = size * 0.6;
-	for (let i = 0; i < 600 + size; i+=offsetH) {
-		for (let j = 0; j < 600; j+=offsetV) {
-			ellipse(i, j, size, size);
-		}
-	}
-
-	ship.pos.x += 8;
-	ship.show();
-
+	game.show();
 	noLoop();
 }
