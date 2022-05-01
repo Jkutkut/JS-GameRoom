bfe8ea5ca1f8dc2d33f09d963cb805ff8df656ac
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-01 17:47:17 +0200

    adding animation logic

--- res/js/space_invaders/model/animations/animation.js
+++ res/js/space_invaders/model/animations/animation.js
@@ -1,8 +1,11 @@
 class Animation {
-	constructor(obj, frames) {
+
+	constructor(obj, frames, frameCooldown) {
 		this._obj = obj;
 		this._currentFrame = 0;
 		this._frames = frames;
+		this._frameCooldown = frameCooldown;
+		this.cooldown = frameCooldown;
 	}
 
 	ended() {
@@ -10,8 +13,43 @@ class Animation {
 	}
 
 	tick() {
-		this._currentFrame++;
+		if (this.cooldown == 0) {
+			this._currentFrame++;
+			this.cooldown = this._frameCooldown;
+		}
+		else
+			this.cooldown--;
+	}
+
+	showFrame(src, imgWidth, scale=1) {
+		if (this.ended())
+			return;
+		push();
+		translate(this.obj.pos.x, this.obj.pos.y);
+		rotate(this.obj.angle);
+		image(
+			src,
+			0, 0,
+			imgWidth * scale, imgWidth * scale,
+			this.frame * imgWidth, 0,
+			imgWidth, imgWidth
+		);
+		pop();
 	}
 
 	show() {}
+
+	// GETTERS
+
+	get obj() {
+		return this._obj;
+	}
+
+	get frame() {
+		return this._currentFrame;
+	}
+
+	get frameCooldown() {
+		return this._frameCooldown;
+	}
 }
--- res/js/space_invaders/model/animations/shipExplosionAnimation.js
+++ res/js/space_invaders/model/animations/shipExplosionAnimation.js
@@ -1,16 +1,21 @@
 class ShipExplosionAnimation extends Animation {
 	static SPRITES = null;
 	static N_SPRITES = 7;
+	static FRAME_COOLDOWN = 10;
 
 	static IMG_WIDTH = 48;
 
 	constructor(obj, frames) {
 		if (!(obj instanceof Ship))
 			throw new Error("Error: ShipExplosionAnimation needs a Ship object");
-		super(obj, frames);
+		super(obj, frames, ShipExplosionAnimation.FRAME_COOLDOWN);
 	}
 
 	show() {
-		
+		this.showFrame(
+			ShipExplosionAnimation.SPRITES,
+			ShipExplosionAnimation.IMG_WIDTH,
+			1.8
+		);
 	}
 }
