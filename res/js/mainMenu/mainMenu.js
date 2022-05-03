75acc52ce122b888a1f78953e2ddd4ee03fab469
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-03 12:49:07 +0200

    games loaded into menuhtml usign js

--- res/js/mainMenu/mainMenu.js
+++ res/js/mainMenu/mainMenu.js
@@ -4,6 +4,30 @@ window.onload = () => {
 		.then(json => loadGames(json));
 }
 
+function newGame(name, location, thumbnail, description) {
+	return `<div class="game">
+			<h3>${name}</h3>
+			<a href="${location}">
+				<img src="${thumbnail}" alt="Thumbnail ${name}">
+			</a>
+			<p>${description}</p>
+			</div>`;
+}
+
 function loadGames(json) {
-	console.log(json);
+	// console.log(json);
+
+	let gameSelector = document.getElementById("gameSelector");
+	let games = json.games;
+
+	let debugTh = games[2].thumbnail; // TODO DEBUG
+	for (let game of games) {
+		gameSelector.innerHTML += newGame(
+			game.name,
+			game.location,
+			// `../${json.thumbnailLocation}/${game.thumbnail}`,
+			`../${json.thumbnailLocation}${debugTh}`, // TODO DEBUG
+			game.description
+		);
+	}
 }
