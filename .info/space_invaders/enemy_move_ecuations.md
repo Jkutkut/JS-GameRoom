f3adf8d3ca669ab95e89fb1acfc980d8d0ae684b
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-05 11:11:23 +0200

    bessier logic implemented as test

--- .info/space_invaders/enemy_move_ecuations.md
+++ .info/space_invaders/enemy_move_ecuations.md
@@ -1,1 +1,5 @@
 https://www.geogebra.org/m/g4veutvu
+
+https://editor.p5js.org/codingtrain/sketches/kZ8dpklQg
+
+https://github.com/peilingjiang/p5.bezier/blob/main/src/p5.bezier.js
--- res/js/space_invaders/space_invaders.js
+++ res/js/space_invaders/space_invaders.js
@@ -18,14 +18,48 @@ function preload() {
 function setup() {
 	createCanvas(600, 600);
 	imageMode(CENTER);
-	game = new SpaceInvaders(new p5.Vector(width, height));
-	
+	// game = new SpaceInvaders(new p5.Vector(width, height));
+	bessier(
+		150,
+		new p5.Vector(0, 0),
+		new p5.Vector(300, 0),
+		new p5.Vector(300, 300),
+		new p5.Vector(0, 300),
+		new p5.Vector(0, 300),
+		new p5.Vector(0, 300),
+		new p5.Vector(0, 200),
+		new p5.Vector(600, 600),
+		new p5.Vector(600, 600),
+		new p5.Vector(600, 600)
+	);
 }
 
 function draw() {
-	game.tick();
+	// game.tick();
 }
 
 keyPressed = () => {
 	game.keypress(keyCode);
+}
+
+function bessier(steps, ...points) {
+	const tIncrement = 1 / steps;
+	const factorial = (n) => {
+		if (n <= 1) return 1;
+		return n * factorial(n - 1);
+	}
+	const nFactorial = factorial(points.length);
+	let x, y, t, i;
+	let factor;
+	for (t = 0; t <= 1; t += tIncrement) {
+		x = 0;
+		y = 0;
+		for (i = 0; i < points.length; i++) {
+			factor = nFactorial / (factorial(i) * factorial(points.length - i))
+					* Math.pow(1 - t, points.length - i) * Math.pow(t, i);
+			x += points[i].x * factor;
+			y += points[i].y * factor;
+		}
+		ellipse(x, y, 10);
+	}
 }
