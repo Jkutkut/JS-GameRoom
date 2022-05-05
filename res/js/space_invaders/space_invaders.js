d99169dadb46772b3a56d283bd9a85f91752123b
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-05 12:57:19 +0200

    logic works with arrays

--- res/js/space_invaders/space_invaders.js
+++ res/js/space_invaders/space_invaders.js
@@ -16,28 +16,69 @@ function preload() {
 }
 
 function setup() {
-	createCanvas(600, 600);
+	// createCanvas(600, 600);
+	let c = createCanvas(1000, 1000);
+	p5bezier.initBezier(c);
 	imageMode(CENTER);
 	// game = new SpaceInvaders(new p5.Vector(width, height));
+
+
+	// bessier(
+	// 	150,
+	// 	true,
+	// 	new p5.Vector(0, 0),
+	// 	new p5.Vector(300, 0),
+	// 	new p5.Vector(300, 300),
+	// 	new p5.Vector(300, 300),
+	// 	new p5.Vector(0, 100),
+	// 	new p5.Vector(0, 120),
+	// 	new p5.Vector(0, 140),
+	// 	new p5.Vector(0, 160),
+	// 	new p5.Vector(100, 600),
+	// 	new p5.Vector(120, 600),
+	// 	new p5.Vector(140, 600),
+	// 	new p5.Vector(160, 600),
+	// 	new p5.Vector(600, 580),
+	// 	new p5.Vector(600, 560),
+	// 	new p5.Vector(600, 540),
+	// 	new p5.Vector(600, 520)
+	// );
+
+	// bessier(
+	// 	300,
+	// 	true,
+	// 	new p5.Vector(10, 10),
+	// 	new p5.Vector(300, 0),
+	// 	new p5.Vector(300, 300)
+	// );
+
+	p5bezier.newBezier([
+		[10, 10],
+		[100, 700],
+		[500, -800],
+		[800, 1000],
+		[10, 300]
+	], 'OPEN');
+	
 	bessier(
-		150,
+		300,
 		true,
-		new p5.Vector(0, 0),
-		new p5.Vector(300, 0),
-		new p5.Vector(300, 300),
-		new p5.Vector(300, 300),
-		new p5.Vector(0, 100),
-		new p5.Vector(0, 120),
-		new p5.Vector(0, 140),
-		new p5.Vector(0, 160),
-		new p5.Vector(100, 600),
-		new p5.Vector(120, 600),
-		new p5.Vector(140, 600),
-		new p5.Vector(160, 600),
-		new p5.Vector(600, 580),
-		new p5.Vector(600, 560),
-		new p5.Vector(600, 540),
-		new p5.Vector(600, 520)
+		new p5.Vector(10, 10),
+		new p5.Vector(10, 10),
+		new p5.Vector(100, 700),
+		new p5.Vector(500, -800),
+		new p5.Vector(800, 1000),
+		new p5.Vector(10, 300)
+	);
+
+	bessier(
+		300,
+		true,
+		[10, 10],
+		[100, 700],
+		[500, -800],
+		[800, 1000],
+		[10, 300]
 	);
 }
 
@@ -51,52 +92,60 @@ keyPressed = () => {
 
 function bessier(steps, debug, ...points) {
 	const tIncrement = 1 / steps;
+	const n = points.length - 1;
 	const factorial = (n) => {
 		if (n <= 1) return 1;
 		return n * factorial(n - 1);
 	}
-	const pointRange = (t, nPoints, amount=nPoints) => {
-		let percent = 1 - t;
-		let index = Math.floor(nPoints * percent);
-		let start = Math.max(0, index - amount);
-		let end = Math.min(nPoints - 1, index + amount);
-		return {start: start, end: end};
-	}
+	// const pointRange = (t, nPoints, amount=nPoints) => {
+	// 	let percent = 1 - t;
+	// 	let index = Math.floor(nPoints * percent);
+	// 	let start = Math.max(0, index - amount);
+	// 	let end = Math.min(nPoints - 1, index + amount);
+	// 	return {start: start, end: end};
+	// }
 	const nFactorial = factorial(points.length);
 	let results = [];
 	let x, y, t, i;
 	let factor, point;
-	for (t = 0; t <= 1; t += tIncrement) {
-		x = 0;
-		y = 0;
-		let r = pointRange(t, points.length);
-		for (i = r.start; i < r.end; i++) {
-			factor = nFactorial / (factorial(i) * factorial(points.length - i))
-					* Math.pow(1 - t, points.length - i) * Math.pow(t, i);
-			x += points[i].x * factor;
-			y += points[i].y * factor;
-		}
-		if (debug)
-			ellipse(x, y, 10);
-	}
-	for (t = 0; t <= 1; t += tIncrement) {
-		x = 0;
-		y = 0;
-		for (i = 0; i < points.length; i++) {
-			factor = nFactorial / (factorial(i) * factorial(points.length - i))
-					* Math.pow(1 - t, points.length - i) * Math.pow(t, i);
-			x += points[i].x * factor;
-			y += points[i].y * factor;
-		}
-		if (debug)
-			ellipse(x, y, 10);
-	}
 	if (debug) {
 		push();
 		stroke(255, 0, 0);
 		fill(255, 0, 0);
 		for (i = 0; i < points.length; i++)
-			ellipse(points[i].x, points[i].y, 20);
+			ellipse(points[i].x, points[i].y, 25);
 		pop();
+	}
+	// for (t = 0; t <= 1; t += tIncrement) {
+	// 	x = 0;
+	// 	y = 0;
+	// 	for (i = 0; i < points.length; i++) {
+	// 		factor = nFactorial / (factorial(i) * factorial(points.length - i))
+	// 				* Math.pow(1 - t, points.length - i) * Math.pow(t, i);
+	// 		x += points[i].x * factor;
+	// 		y += points[i].y * factor;
+	// 	}
+	// 	if (debug)
+	// 		ellipse(x, y, 10);
+	// }
+	let xy, d;
+
+	for (t = 0; t <= 1; t += tIncrement) {
+		xy = [0, 0];
+		console.log(xy);
+		for (i = 0; i <= n; i++) {
+			for (d = 0; d < 2; d++) {
+				xy[d] +=
+					(factorial(n) /
+					(factorial(i) * factorial(n - i))) *
+					Math.pow(1 - t, n - i) *
+					Math.pow(t, i) *
+					points[i][d];
+			}
+		}
+		if (debug) {
+			ellipse(...xy, 20);
+			console.log(xy);
+		}
 	}
 }
