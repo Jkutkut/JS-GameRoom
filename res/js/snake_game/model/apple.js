489a1f2dbf4b58b15093b7302f0910f2ebfe540e
mariadv_ <95478604+mariaduva@users.noreply.github.com>
2022-05-07 18:13:48 +0200

    now the apple appers in the middle of the squares

--- res/js/snake_game/model/apple.js
+++ res/js/snake_game/model/apple.js
@@ -19,18 +19,36 @@ class Apple {
 
 var circle_hl = 30;
 var canvas_hl = 600;
+var y = 0;
+var x = 0;
+var cont_rect = 0;
 
 function setup() { 
   createCanvas(canvas_hl, canvas_hl);
   noStroke();
   background(220);
+  drawBackgroundPattern();
   drawRdCircle();
 } 
 
-
 function drawRdCircle() {
-  let x_circle = random(canvas_hl - circle_hl);
-  let y_circle = random(canvas_hl - circle_hl);
+    
+  let x_circle = (int(random(0, 20)) * 30 ) +15;
+  let y_circle = (int(random(0, 20)) * 30 ) +15;
+  
   fill('#F44336');
-  ellipse(x_circle,y_circle,circle_hl,circle_hl);
+  ellipse(x_circle, y_circle, circle_hl);
+}
+
+function drawBackgroundPattern(){
+  	for (let x = 0; x < 20; x++) {
+		for (let y = 0; y < 20; y++) {
+			if ((y + x * 19) % 2 == 0)
+				fill(255);
+			else
+				fill(120);
+			rect(x * 30, y * 30, 30, 30);
+		}
+		
+	}
 }
