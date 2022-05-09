ea4bdf3b9eb3837bceb126310c5aece2ade6f816
luciia23 <l.colladomartinrq@gmail.com>
2022-05-09 11:38:01 +0200

    pruebas random

--- res/js/pong_game/pong_game.js
+++ res/js/pong_game/pong_game.js
@@ -1,8 +1,42 @@
+let player;
+let bola;
 function setup() {
     createCanvas(600, 600);
     background(0);
+    strokeWeight(2)
+    fill(255);
+    imageMode(CENTER);
+
+    player = new Jugador(mouseX, mouseY - 35);
+    bola = new Ball(width / 2, height / 2, 20);
+
 }
 
 function draw() {
-    circle(mouseX, mouseY, 80);
+    background(0);
+    fill(255);
+    player.show();
+    player.update(600);
+    bola.show();
+    bola.update();
+    bola.checkBoundaryCollision();
 }
+
+function keyPressed() {
+    if (keyCode == LEFT_ARROW) {
+        value == 255;
+    }
+    if (keyCode == RIGTH_ARROW) {
+        value == 0;
+    }
+
+}
+
+
+/*fill(255);
+rect(600, mouseY - 35, 40, 70, 20);*/
+
+
+    // new Ball(100, 400, 20);
+
+
