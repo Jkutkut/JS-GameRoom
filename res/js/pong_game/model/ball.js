be940714b3c8f093d3ff9c5e7e987a43c2a72652
luciia23 <l.colladomartinrq@gmail.com>
2022-05-09 11:37:29 +0200

    Create ball.js

---
+++ res/js/pong_game/model/ball.js
@@ -0,0 +1,32 @@
+class Ball {
+  constructor(x, y, r) {
+    this.position = new p5.Vector(x, y);
+    this.velocity = p5.Vector.random2D();
+    this.velocity.mult(3);
+    this.r = r;
+    this.m = r * 0.1;
+  }
+  update() {
+    this.position.add(this.velocity);
+  }
+  show() {
+    fill(255);
+    ellipse(this.position.x, this.position.y, this.r * 2);
+  }
+
+  checkBoundaryCollision() {
+    if (this.position.x > width - this.r) {
+      this.position.x = width - this.r;
+      this.velocity.x *= -1;
+    } else if (this.position.x < this.r) {
+      this.position.x = this.r;
+      this.velocity.x *= -1;
+    } else if (this.position.y > height - this.r) {
+      this.position.y = height - this.r;
+      this.velocity.y *= -1;
+    } else if (this.position.y < this.r) {
+      this.position.y = this.r;
+      this.velocity.y *= -1;
+    }
+  }
+}
