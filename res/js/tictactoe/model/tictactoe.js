fadeb270131fa1295f49730f07c95ce7214849e5
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-22 16:47:27 +0200

    Now game should stop when someone wons

--- res/js/tictactoe/model/tictactoe.js
+++ res/js/tictactoe/model/tictactoe.js
@@ -1,7 +1,7 @@
 class Tictactoe {
 
-	static UNDEFINED = -1;
-	static CROSS = 0;
+	static UNDEFINED = 0;
+	static CROSS = -1;
 	static CIRCLE = 1;
 
 	constructor(board) {
@@ -17,13 +17,7 @@ class Tictactoe {
 		this.click(1, 1);
 		this.click(0, 2);
 		this.click(2, 2);
-
-		Tictactoe.markAsSolution(this.UIboard[0][0]);
-		Tictactoe.markAsSolution(this.UIboard[1][1]);
-		Tictactoe.markAsSolution(this.UIboard[2][2]);
-		Tictactoe.markAsSolution(this.UIboard[2][0]);
-		this._running = false;
-		this.updateUI();
+		this.click(2, 1); // Does not execute
 	}
 
 	initBoard(board) {
@@ -86,11 +80,58 @@ class Tictactoe {
 			Tictactoe.select(Tictactoe.getCircle(this.UIboard[x][y]));
 			this.turn = Tictactoe.CROSS;
 		}
+		this.checkBoard();
 		this.updateUI();
 	}
 
 	mouseClick(x, y) {
 		this.click(x, y);
+	}
+
+	checkBoard() {
+		let winner = Tictactoe.UNDEFINED;
+		let solCells = [];
+		for (let i = 0, j, row, col; i < 3; i++) {
+			row = 0;
+			col = 0;
+			for (j = 0; j < 3; j++) {
+				row += this.board[i][j];
+				col += this.board[j][i];
+			}
+			if (row == Tictactoe.CIRCLE * 3 || col == Tictactoe.CIRCLE * 3) {
+				winner = Tictactoe.CIRCLE;
+				solCells = this.board[i];
+				break;
+			}
+			else if (row == Tictactoe.CROSS * 3 || col == Tictactoe.CROSS * 3) {
+				winner = Tictactoe.CROSS;
+				for (j = 0; j < 3; j++)
+					solCells.push(this.UIboard[j][i]);
+				break;
+			}
+		}
+		if (winner == Tictactoe.UNDEFINED) {
+			// Diagonals
+			if (this.board[0][0] + this.board[1][1] + this.board[2][2] == 3) {
+				winner = Tictactoe.CROSS;
+				solCells = [this.UIboard[0][0], this.UIboard[1][1], this.UIboard[2][2]];
+			}
+			else if (this.board[0][0] + this.board[1][1] + this.board[2][2] == -3) {
+				winner = Tictactoe.CIRCLE;
+				solCells = [this.UIboard[0][0], this.UIboard[1][1], this.UIboard[2][2]];
+			}
+		}
+
+		if (winner != Tictactoe.UNDEFINED) {
+			if (winner == Tictactoe.CROSS)
+				console.log("Cross wins");
+			else
+				console.log("Circle wins");
+			for (let cell of solCells)
+				Tictactoe.markAsSolution(cell);
+			this._running = false;
+		}
+
 	}
 
 	// UI logic
