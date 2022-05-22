7d266e9c2a0f41272651bc3d83cbe180a5f765f7
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-22 18:05:50 +0200

    testRow added

--- res/js/tictactoe/test/testtictactoe.js
+++ res/js/tictactoe/test/testtictactoe.js
@@ -12,7 +12,9 @@ class TestResult {
 class TestTictactoe {
 
 	static TESTS = [
-		"test_inits"
+		"test_inits",
+		"test_runs",
+		"test_win_row"
 	];
 
 	static test() {
@@ -20,7 +22,7 @@ class TestTictactoe {
 			let result = TestTictactoe[TestTictactoe.TESTS[i]]();
 			if (result.failed) {
 				console.error(`${TestTictactoe.TESTS[i]} failed:`);
-				console.error(result.message);
+				console.error(result.msg);
 				console.error("");
 				return;
 			}
@@ -36,6 +38,41 @@ class TestTictactoe {
 			let game = new Tictactoe();
 		} catch (e) {
 			return new TestResult(true, e);
+		}
+		return new TestResult(false);
+	}
+
+	static test_runs() {
+		let game = new Tictactoe();
+		let runs = game.running;
+		return new TestResult(!runs, `running should be ${true} but is ${runs}`);
+	}
+
+
+	static test_win_row() {
+		return this.test_win_row01();
+	}
+
+	static test_win_row01() {
+		let game = new Tictactoe();
+		/*
+		x x x
+		. . .
+		o o .
+		*/
+		game.click(0, 0);
+		game.click(2, 0);
+		game.click(0, 1);
+		game.click(2, 1);
+		game.click(0, 2);
+		game.click(2, 2);
+		let runs = game.running;
+		if (runs)
+			return new TestResult(true, "Game should be over");
+		let winner = game.checkBoard();
+		for (let i = 0; i < 3; i++) {
+			if (winner[i][1] != 0)
+				return new TestResult(true, "Winner should be on row 0 but is " + winner[i][1]);
 		}
 		return new TestResult(false);
 	}
