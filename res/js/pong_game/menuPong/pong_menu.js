d4ecf81d29c87325821e5234a411f4608e37c5d8
luciia23 <l.colladomartinrq@gmail.com>
2022-05-29 19:25:03 +0200

    Delete pong_menu.js

--- res/js/pong_game/menuPong/pong_menu.js
+++
@@ -1,9 +0,0 @@
-
-  function change() {
-    const className =
-        document.getElementById('link').className;
-    document.getElementById('link').className =
-        document.getElementById('adios').className;
-
-    document.getElementById('adios').className = className;
-}
